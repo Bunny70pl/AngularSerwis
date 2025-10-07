9fed921f91342488ff4b6d1dac871558b11eee26
bunny70pl <bunny70pl@gmail.com>
2025-10-07 09:42:37 +0200

    dziala

---
+++ src/app/osoby.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Osoba } from './osoba';
+import { Plec } from './osoba';
+@Injectable({
+  providedIn: 'root'
+})
+export class OsobyService {
+
+  constructor() { }
+  ludzie:Osoba[] = [{
+    id:1,
+    imie:'Ala',
+    nazwisko:'Nowak',
+    dataUr:"2005-05-25",
+    zdjecie:"ala.jpg",
+    waga:60,
+    wzrost:160,
+    plec: Plec.K
+  },
+  {
+    id:2,
+    imie:'Ola',
+    nazwisko:'Kowalska',
+    dataUr:"2011-01-01",
+    zdjecie:"ola.jpg",
+    waga:58,
+    wzrost:161,
+    plec: Plec.K
+  }
+  ,
+  {
+    id:3,
+    imie:'Olek',
+    nazwisko:'Kowalski',
+    dataUr:"2005-05-25",
+    zdjecie:"olek.jpg",
+    waga:80,
+    wzrost:185,
+    plec: Plec.M
+  },
+  {
+      id:4,
+    imie:'Romek',
+    nazwisko:'Rowalski',
+    dataUr:"2001-02-25",
+    zdjecie:"romek.jpg",
+    waga:90,
+    wzrost:195,
+    plec: Plec.M
+  }];
+  getLudzie(){
+    return this.ludzie
+  }
+}
--- src/app/osoby/osoby.component.ts
+++ src/app/osoby/osoby.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Osoba } from '../osoba';
 import { Plec } from '../osoba';
+import { OsobyService } from '../osoby.service';
+
 @Component({
   selector: 'app-osoby',
   standalone: true,
@@ -9,45 +11,8 @@ import { Plec } from '../osoba';
   styleUrl: './osoby.component.css'
 })
 export class OsobyComponent {
-ludzie:Osoba[] = [{
-  id:1,
-  imie:'Ala',
-  nazwisko:'Nowak',
-  dataUr:"2005-05-25",
-  zdjecie:"ala.jpg",
-  waga:60,
-  wzrost:160,
-  plec: Plec.K
-},
-{
-  id:2,
-  imie:'Ola',
-  nazwisko:'Kowalska',
-  dataUr:"2011-01-01",
-  zdjecie:"ola.jpg",
-  waga:58,
-  wzrost:161,
-  plec: Plec.K
+ludzie:Osoba[]=[];
+constructor( private serwis:OsobyService){
+  this.ludzie = this.serwis.getLudzie();
 }
-,
-{
-  id:3,
-  imie:'Olek',
-  nazwisko:'Kowalski',
-  dataUr:"2005-05-25",
-  zdjecie:"olek.jpg",
-  waga:80,
-  wzrost:185,
-  plec: Plec.M
-},
-{
-    id:4,
-  imie:'Romek',
-  nazwisko:'Rowalski',
-  dataUr:"2001-02-25",
-  zdjecie:"romek.jpg",
-  waga:90,
-  wzrost:195,
-  plec: Plec.M
-}];
 }
